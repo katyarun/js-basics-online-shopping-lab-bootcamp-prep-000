ad11afef3d42e942a4c844cd2ec5f80a4057b46b
katyarun@gmail.com <katyarun@gmail.com>
2018-05-16 03:24:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  cart.push(Object.assign({}, { item : Math.floor(Math.random())}));
+  cart.push(Object.assign({}, { [item] : Math.floor(Math.random())}));
   return `${item} has been added to your cart.`;
 }
 
