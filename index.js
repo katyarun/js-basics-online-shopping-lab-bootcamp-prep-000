8562d0f21cc4fa1c6775656a2a7b4765ecb2d427
katyarun@gmail.com <katyarun@gmail.com>
2018-05-16 04:05:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    const str = `Ypur total cost is ${total()}, which will be charged to the card ${cardNumber}.`;
+    const str = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
     cart = [];
     return str;
   }
